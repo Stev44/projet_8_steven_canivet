16ffc0da2aa1f873f85a4e69cfa9f74c48645729
Steven Canivet <canivet.steven@gmail.com>
2024-06-03 20:11:31 +0200

    dqz

--- kasa/src/pages/about/about.jsx
+++ kasa/src/pages/about/about.jsx
@@ -1,10 +1,22 @@
 import Banner from '../../components/banner/banner'
+import Collapse from '../../components/collapse/collapse'
+import aboutData from '../../datas/about'
+import './about.scss'
 
 function About() {
   return (
-    <div>
+    <body>
       <Banner />
-    </div>
+      <main>
+        {aboutData.map((data) => {
+          return (
+            <div key={data.id} className="about_collapse">
+              <Collapse title={data.title} content={data.content} />
+            </div>
+          )
+        })}
+      </main>
+    </body>
   )
 }
 
--- kasa/src/pages/accomodation/accomodation.jsx
+++ kasa/src/pages/accomodation/accomodation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import data from '../../datas/data'
 import Collapse from '../../components/collapse/collapse'
