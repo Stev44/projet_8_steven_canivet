62a24a64766937716a527c47671215abeb34b54c
Steven Canivet <canivet.steven@gmail.com>
2024-09-02 17:43:03 +0200

    qd

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/home';
-import Accomodation from './pages/accomodation/accomodation';
-import About from './pages/about/about';
-import Error from './pages/error/error';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import './style/main.scss';
+import Home from './pages/home/home'
+import Accomodation from './pages/accomodation/accomodation'
+import About from './pages/about/about'
+import Error from './pages/error/error'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import './style/main.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-     <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,6 +23,4 @@ root.render(
       <Footer />
     </Router>
   </React.StrictMode>
-);
-
-
+)
